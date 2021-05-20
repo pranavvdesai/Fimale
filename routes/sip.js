54fa15b1f5f15a9349a2d8c8be44c5288b74197b
pranav desai <74852751+pranavvdesai@users.noreply.github.com>
2021-05-21 04:37:22 +0530

    Update sip.js

--- routes/sip.js
+++ routes/sip.js
@@ -3,10 +3,6 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
 	res.render("sip");
-	// res.render("sip");
 });
-// router.route("/").get((req, res) => {
-// 	res.render("sip");
-// });
 
 module.exports = router;
